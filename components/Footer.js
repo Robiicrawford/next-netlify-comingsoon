fcc0be1b19325473029b1b2288f3d2715c248eba
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2023-03-21 15:00:03 -0400

    change logo

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/278112982_526440742431144_5243909603411308321_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mttNekcKwlIAX-U2Yty&_nc_ht=scontent-ord5-2.xx&oh=00_AfAO132UQShWXVearYNJFiHYP9b5l20hWhPle5eZqkvWuw&oe=641E11EF" alt="Netlify Logo" className={styles.logo} />
       </footer>
     </>
   )
